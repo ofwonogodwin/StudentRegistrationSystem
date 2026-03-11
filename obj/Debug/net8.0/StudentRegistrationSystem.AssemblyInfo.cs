a1aa1bf8e2cb5291df1ba9ea06c758b9102b4215
Godwin Ofwono <godwinofwono933@gmail.com>
2026-03-11 17:16:35 +0300

    Changed fron username for signin to email

--- obj/Debug/net8.0/StudentRegistrationSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentRegistrationSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentRegistrationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cda9935029507b169cda41086cca8376fde51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed08681dba0c5ed89cbf817b3ab50f7cacb5992")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentRegistrationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentRegistrationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
